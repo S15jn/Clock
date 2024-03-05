da0f6e988fa8ad0d10cf0e304d2cb52809cb0dd7
Somya Jain <125781681+S15jn@users.noreply.github.com>
2024-03-05 18:23:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+function currentTime(){
+    let date= new Date();
+    let hh=date.getHours();
+    let mm=date.getMinutes();
+    let ss=date.getSeconds();
+    let session ='AM';
+
+if(hh==0){
+    hh=12;
+}
+if(hh>12){
+    hh=hh-12;
+    session="PM";
+}
+
+hh=(hh<10) ? "0" + hh : hh;
+mm=(mm<10) ? "0" + mm : mm;
+ss=(ss<10) ? "0" + ss : ss;
+
+// display format
+let time=hh + ":"+mm +":" +ss + " "+ session;
+
+// create div
+let c=document.getElementById('clock');
+c.innerText=time;
+
+//update clock func
+
+let t= setTimeout( function(){ 
+    currentTime() }, 1000);
+
+
+
+
+}
+currentTime();
